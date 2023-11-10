1ed743f4755af49cfb24cae08969e3422a45c946
Niklas Tewes <niklas.tewes@hegla.de>
2023-11-10 08:56:10 +0100

    Added first test case

--- cuttingpattern/tests.py
+++ cuttingpattern/tests.py
@@ -1,3 +1,18 @@
 from django.test import TestCase
+from .api import Api
+
+import pandas
 
 # Create you tests here
+class APITestCase(TestCase):
+
+    def setUp(self) -> None:
+        pass
+
+    def test_unresolved_api_command(self):
+        result = Api().resolve_command("unkown_command", None)
+
+        self.assertJSONEqual(
+            result.content.decode("utf-8"),
+            {"result": False, "error": "Command is not defined!"}
+        )
